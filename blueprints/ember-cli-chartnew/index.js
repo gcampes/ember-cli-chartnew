529503f6128e4c2fe37086602acc7595d73f9b72
gcampes <gabrieldecampes@gmail.com>
2015-12-09 09:52:14 -0200

    Fix issue on blueprint with entity name

--- blueprints/ember-cli-chartnew/index.js
+++ blueprints/ember-cli-chartnew/index.js
@@ -1,12 +1,11 @@
-module.exports = {
-  description: '',
+'use strict'
 
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
+module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
 
   afterInstall: function() {
     var _this = this;
