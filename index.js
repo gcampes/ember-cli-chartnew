4a6d5a578246168d2b4fd7e0e6d2b49b76b2d668
gcampes <gabrieldecampes@gmail.com>
2015-12-09 10:07:47 -0200

    Change index.js

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-chartnew'
+  name: 'ember-cli-chartnew',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/chartnew-js/ChartNew.js');
+  },
 };
